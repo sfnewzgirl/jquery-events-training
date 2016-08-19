e5f71e136b8af1ac726719719868677c05b1b1e0
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-08-18 22:44:56 -0700

    starts clock when spacebar pressed and held

--- starter-code/keypress/base.js
+++ starter-code/keypress/base.js
@@ -1,5 +1,21 @@
 console.log("Sanity Check: JS is working!");
 
+var startTime;
+var endTime;
+var totalTime;
+
 $(document).ready(function(){
+
+  $(window).on("keypress", function handleKeypress(event){
+
+    if ( startTime ) {
+      endTime = Date.now();
+      totalTime = (endTime - startTime) / 1000;
+      $("#total-time").text( totalTime + " seconds" )
+    }  else {
+      startTime = Date.now();
+    }
+
+  })
 
 })
