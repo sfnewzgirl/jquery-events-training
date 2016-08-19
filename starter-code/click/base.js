2b2f2bc763e722624ad577a75bbfd9665db2eb50
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-08-18 22:16:28 -0700

    creates function to add li to ul of clicked phrases

--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -2,4 +2,8 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
+  $("span").on("click", function handleClick(event){
+  var clickedExclamation = "You clicked: " + $(this).text() + " at " + Date.now();
+  $("ul").append("<li>" + clickedExclamation + "</li>")
+  })
 })
